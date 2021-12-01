750af5c5df0133d91c0f04d663e356b0939f0af8
richardcoldwell74 <76210848+richardcoldwell74@users.noreply.github.com>
2021-12-01 09:51:32 +0000

    Day 1

---
+++ pages/Day_01/index.tsx
@@ -0,0 +1,72 @@
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import { input } from './input';
+import { testInput } from './testInput';
+import styled from 'styled-components';
+import { useState } from 'react';
+
+
+
+
+const findGreaterDepths = (
+  inputArray: number[],
+): number => {
+  let count = 0
+  let lastDepth = inputArray[0];
+  for (const depth of inputArray) {
+    if (depth > lastDepth) {
+      count++ ;
+    }
+    lastDepth = depth;
+  }
+  return count
+}
+
+
+const findGreaterDepthsGrouped = (
+  inputArray: number[],
+): number => {
+  let count = 0
+  let depth = 0;
+  let lastDepth = inputArray[0] + inputArray[1] + inputArray[2];
+  for (let index = 0; index < inputArray.length; index++) {
+    if (index < inputArray.length - 2)
+    depth = inputArray[index] + inputArray[index + 1] + inputArray[index + 2]
+    {
+      if (depth > lastDepth) {
+        count++ ;
+      }
+      lastDepth = depth;
+    }    
+  }
+  return count;
+}
+
+const getInput = (): number[] => input.split('\n').map(Number)
+
+export default function Home() {
+
+  return (
+    <div>
+      <Head>
+        <title>AOC 2021 - Day 01</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Advent Of Code Day 01" />
+        <h2>Part 1</h2>
+        <p >
+         {findGreaterDepths(getInput())}
+        </p>
+        <h2>Part 2</h2>
+        <p >
+         {findGreaterDepthsGrouped(getInput())}
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
+import styled from 'styled-components';
 
 export default function Home() {
   return (
@@ -12,9 +13,12 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <p >
+        You're minding your own business on a ship at sea when the overboard alarm goes off! You rush to see if you can help. Apparently, one of the Elves tripped and accidentally sent the sleigh keys flying into the ocean!
         </p>
+        <p>Before you know it, you're inside a submarine the Elves keep ready for situations like this. It's covered in Christmas lights (because of course it is), and it even has an experimental antenna that should be able to track the keys if you can boost its signal strength high enough; there's a little meter that indicates the antenna's signal strength by displaying 0-50 stars.</p>
+        <p>Your instincts tell you that in order to save Christmas, you'll need to get all fifty stars by December 25th.</p>
+        <p>Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!</p>
       </main>
 
       <Footer />
