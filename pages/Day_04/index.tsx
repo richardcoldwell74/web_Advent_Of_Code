2c1de0f4a730b6d1987cd313daf04170bdd47d29
FrozenWellStudios <richardcoldwell74@gmail.com>
2021-12-05 20:52:57 +0000

    Day 4 part 1 refactor

--- pages/Day_04/index.tsx
+++ pages/Day_04/index.tsx
@@ -6,7 +6,6 @@ import { testNumbers } from "../../api/day_04/testNumbers";
 import { BoardsInput } from "../../api/day_04/BoardsInput";
 import { NumbersInput } from "../../api/day_04/NumbersInput";
 import styled from "styled-components";
-import { useState } from "react";
 
 const Container = styled.div`
   height: 100vh;
@@ -19,86 +18,8 @@ const Container = styled.div`
   background-image: url("winter_background.png");
 `;
 
-const CheckIfWinningLine = (boardInput) => {
-  //is it a winning line across
-  //loop through each board
-  for (let BoardIndex = 0; BoardIndex < boardInput.length; BoardIndex++) {
-    //loop through each row of the board
-    for (
-      let BoardLineIndex = 0;
-      BoardLineIndex < boardInput[BoardIndex].length;
-      BoardLineIndex++
-    ) {
-      //loop through each number in the row
-      let lineTotal = 0;
-      for (
-        let BoardNumberIndex = 0;
-        BoardNumberIndex < boardInput[BoardIndex][BoardLineIndex].length;
-        BoardNumberIndex++
-      ) {
-        //add number to lineTotal
-        lineTotal += boardInput[BoardIndex][BoardLineIndex][BoardNumberIndex];
-      }
-
-      if (lineTotal === 500) {
-        return BoardIndex;
-      }
-    }
-  }
-
-  return -1;
-};
-
-const CheckIfWinningColumn = (boardInput) => {
-  //is it a winning line down
-  //loop through each board
-  for (let BoardIndex = 0; BoardIndex < boardInput.length; BoardIndex++) {
-    //loop though each column of the board
-
-    for (let BoardNumberIndex = 0; BoardNumberIndex < 5; BoardNumberIndex++) {
-      let columnTotal = 0;
-
-      columnTotal += boardInput[BoardIndex][0][BoardNumberIndex];
-      columnTotal += boardInput[BoardIndex][1][BoardNumberIndex];
-      columnTotal += boardInput[BoardIndex][2][BoardNumberIndex];
-      columnTotal += boardInput[BoardIndex][3][BoardNumberIndex];
-      columnTotal += boardInput[BoardIndex][4][BoardNumberIndex];
-
-      if (columnTotal === 500) {
-        return BoardIndex;
-      }
-    }
-  }
-  return -1;
-};
-
-//pass in array of boards and index of specific board and calculates totla of numbers not checked off
-const RestOfBoardValue = (boardInput, BoardIndex) => {
-  let BoardRemainingTotal = 0;
-  //loop through each row of the board
-  for (
-    let BoardLineIndex = 0;
-    BoardLineIndex < boardInput[BoardIndex].length;
-    BoardLineIndex++
-  ) {
-    //loop through each number in the row
-    for (
-      let BoardNumberIndex = 0;
-      BoardNumberIndex < boardInput[BoardIndex][BoardLineIndex].length;
-      BoardNumberIndex++
-    ) {
-      //add number to BoardRemainingTotal if not equal to 100
-      if (boardInput[BoardIndex][BoardLineIndex][BoardNumberIndex] != 100) {
-        BoardRemainingTotal +=
-          boardInput[BoardIndex][BoardLineIndex][BoardNumberIndex];
-      }
-    }
-  }
-  return BoardRemainingTotal;
-};
-
+// const getInputNumbers = (): number[] => testNumbers.split(",").map(Number);
 const getInputNumbers = (): number[] => NumbersInput.split(",").map(Number);
-// const getInputNumbers = (): number[] => NumbersInput.split(",").map(Number);
 
 const getInputBoards = () =>
   // BoardsInput.split("\n")
@@ -118,37 +39,16 @@ const CalculateAnswerPartOne = () => {
   let answer = 0;
   let numbersInput = getInputNumbers();
   let boardInput = getInputBoards();
+
   //loop through each number called
-  // numbersInput.forEach((calledNumber) => {
-  for (let numberIndex = 0; numberIndex < numbersInput.length; numberIndex++) {
-    //loop through each Board
-    for (let BoardIndex = 0; BoardIndex < boardInput.length; BoardIndex++) {
-      //loop through each row of the board
-      for (
-        let BoardLineIndex = 0;
-        BoardLineIndex < boardInput[BoardIndex].length;
-        BoardLineIndex++
-      ) {
-        //loop through each number in the row
-        for (
-          let BoardNumberIndex = 0;
-          BoardNumberIndex < boardInput[BoardIndex][BoardLineIndex].length;
-          BoardNumberIndex++
-        ) {
-          //compare called number with number on board and if a match set value to 100
-          if (
-            numbersInput[numberIndex] ===
-            boardInput[BoardIndex][BoardLineIndex][BoardNumberIndex]
-          ) {
-            boardInput[BoardIndex][BoardLineIndex][BoardNumberIndex] = 100;
-          }
-        }
-      }
-      let returnValue = CheckIfWinningLine(boardInput);
-      let returnValueColumn = CheckIfWinningColumn(boardInput);
-      if (returnValue != -1 || returnValueColumn != -1) {
-        answer =
-          RestOfBoardValue(boardInput, BoardIndex) * numbersInput[numberIndex];
+  for (let calledNumber of numbersInput) {
+    //mark off the current number from the board
+    boardInput = markBoards(boardInput, calledNumber);
+    for (let board of boardInput) {
+      let ItsAWinner = constCheckIfBoardIsALineWinner(board);
+      let ItsAColumnWinner = CheckIfWinningColumnTOUPDATE(board);
+      if (ItsAWinner || ItsAColumnWinner) {
+        answer = RestOfBoardValueTOUPDATE(board) * calledNumber;
         return answer;
       }
     }
@@ -158,62 +58,66 @@ const CalculateAnswerPartOne = () => {
 
 const CalculateAnswerPartTwo = () => {
   let answer = 0;
-  let numbersInput = getInputNumbers();
-  let boardInput = getInputBoards();
-  let winningBoards = [];
-  const totalBoards = boardInput.length;
-  //loop through each number called
-  for (let numberIndex = 0; numberIndex < numbersInput.length; numberIndex++) {
-    //loop through each Board
-    for (let BoardIndex = 0; BoardIndex < boardInput.length; BoardIndex++) {
-      //loop through each row of the board
-      for (
-        let BoardLineIndex = 0;
-        BoardLineIndex < boardInput[BoardIndex].length;
-        BoardLineIndex++
-      ) {
-        //loop through each number in the row
-        for (
-          let BoardNumberIndex = 0;
-          BoardNumberIndex < boardInput[BoardIndex][BoardLineIndex].length;
-          BoardNumberIndex++
-        ) {
-          //compare called number with number on board and if a match set value to 100
-          if (
-            numbersInput[numberIndex] ===
-            boardInput[BoardIndex][BoardLineIndex][BoardNumberIndex]
-          ) {
-            boardInput[BoardIndex][BoardLineIndex][BoardNumberIndex] = 100;
-          }
-        }
-      }
-      let returnValueLine = CheckIfWinningLine(boardInput);
-      if (returnValueLine != -1) {
-        if (!winningBoards.includes(returnValueLine)) {
-          winningBoards.push(returnValueLine);
-        }
-      }
-      let returnValueColumn = CheckIfWinningColumn(boardInput);
-      if (returnValueColumn != -1) {
-        if (!winningBoards.includes(returnValueColumn)) {
-          winningBoards.push(returnValueColumn);
-        }
-      }
-      if (winningBoards.length === totalBoards) {
-        answer =
-          RestOfBoardValue(boardInput, BoardIndex) * numbersInput[numberIndex];
 
-        console.log(answer);
-        return answer;
-      }
-    }
-  }
   return answer;
 };
 
+const markBoards = (boardInput: number[][][], calledNumber: number) => {
+  boardInput = boardInput.map((board) =>
+    board.map((line) =>
+      line.map((number) => {
+        if (number === calledNumber) {
+          return 100;
+        } else {
+          return number;
+        }
+      })
+    )
+  );
+  return boardInput;
+};
+
+const RestOfBoardValueTOUPDATE = (board: number[][]) => {
+  let BoardRemainingTotal = 0;
+  board.forEach((line) => {
+    line.forEach((number) => {
+      if (number != 100) {
+        BoardRemainingTotal += number;
+      }
+    });
+  });
+  return BoardRemainingTotal;
+};
+
+const constCheckIfBoardIsALineWinner = (board: number[][]) => {
+  let returnValue: boolean = false;
+  //loop through each row of the board
+  board.forEach((line) => {
+    let lineTotal = 0;
+    line.forEach((number) => {
+      lineTotal += number;
+    });
+
+    if (lineTotal === 500) {
+      returnValue = true;
+    }
+  });
+  return returnValue;
+};
+
+const CheckIfWinningColumnTOUPDATE = (board: number[][]) => {
+  return constCheckIfBoardIsALineWinner(transpose(board));
+};
+
+function transpose(a) {
+  return Object.keys(a[0]).map(function (c) {
+    return a.map(function (r) {
+      return r[c];
+    });
+  });
+}
+
 export default function Day04() {
-  const [firstAnswer, setFirstAnswer] = useState(CalculateAnswerPartOne);
-  const [secondAnswer, setSecondAnswer] = useState(CalculateAnswerPartTwo());
   return (
     <Container>
       <Head>
@@ -225,9 +129,9 @@ export default function Day04() {
         <Header />
         <h1>Day 4</h1>
         <h2>Part 1</h2>
-        <p>{firstAnswer}</p>
+        <p>{CalculateAnswerPartOne()}</p>
         <h2>Part 2</h2>
-        <p>{secondAnswer}</p>
+        <p>{CalculateAnswerPartTwo()}</p>
       </main>
 
       <Footer />
