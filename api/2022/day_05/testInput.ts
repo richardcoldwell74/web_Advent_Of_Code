cec0156b9545597308bb2f888cf8b351c6a98595
richardcoldwell74 <76210848+richardcoldwell74@users.noreply.github.com>
2022-12-06 22:00:33 +0000

    day 6 done

--- api/2022/day_05/testInput.ts
+++ api/2022/day_05/testInput.ts
@@ -1,1 +1,5 @@
-export const testInput = ``;
+export const testInput = `move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
+
--- pages/2022/Day_05/index.tsx
+++ pages/2022/Day_05/index.tsx
@@ -17,23 +17,160 @@ const Container = styled.div`
   background-image: url("/winter_background.png");
 `;
 
-const dataCreation = (inputArray: string[]) => {};
+type Instruction = { iterations: number; from: number; to: number };
 
-const dayFiveA = (inputArray: string[]): number => {
-  dataCreation(inputArray);
-  let total = 0;
-
-  return total;
+const testCratesArray = (): string[][] => {
+  let testCrates: string[][] = [];
+  let test1: string[] = [];
+  test1.push("Z");
+  test1.push("N");
+  testCrates.push(test1);
+  let test2: string[] = [];
+  test2.push("M");
+  test2.push("C");
+  test2.push("D");
+  testCrates.push(test2);
+  let test3: string[] = [];
+  test3.push("P");
+  testCrates.push(test3);
+  return testCrates;
 };
 
-const dayFiveB = (inputArray: string[]): number => {
-  dataCreation(inputArray);
-  let total = 0;
-
-  return total;
+const cratesArray = (): string[][] => {
+  let testCrates: string[][] = [];
+  let test1: string[] = [];
+  test1.push("L");
+  test1.push("N");
+  test1.push("W");
+  test1.push("T");
+  test1.push("D");
+  testCrates.push(test1);
+  let test2: string[] = [];
+  test2.push("C");
+  test2.push("P");
+  test2.push("H");
+  testCrates.push(test2);
+  let test3: string[] = [];
+  test3.push("W");
+  test3.push("P");
+  test3.push("H");
+  test3.push("N");
+  test3.push("D");
+  test3.push("G");
+  test3.push("M");
+  test3.push("J");
+  testCrates.push(test3);
+  let test4: string[] = [];
+  test4.push("C");
+  test4.push("W");
+  test4.push("S");
+  test4.push("N");
+  test4.push("T");
+  test4.push("Q");
+  test4.push("L");
+  testCrates.push(test4);
+  let test5: string[] = [];
+  test5.push("P");
+  test5.push("H");
+  test5.push("C");
+  test5.push("N");
+  testCrates.push(test5);
+  let test6: string[] = [];
+  test6.push("T");
+  test6.push("H");
+  test6.push("N");
+  test6.push("D");
+  test6.push("M");
+  test6.push("W");
+  test6.push("Q");
+  test6.push("B");
+  testCrates.push(test6);
+  let test7: string[] = [];
+  test7.push("M");
+  test7.push("B");
+  test7.push("R");
+  test7.push("J");
+  test7.push("G");
+  test7.push("S");
+  test7.push("L");
+  testCrates.push(test7);
+  let test8: string[] = [];
+  test8.push("Z");
+  test8.push("N");
+  test8.push("W");
+  test8.push("G");
+  test8.push("V");
+  test8.push("B");
+  test8.push("R");
+  test8.push("T");
+  testCrates.push(test8);
+  let test9: string[] = [];
+  test9.push("W");
+  test9.push("G");
+  test9.push("D");
+  test9.push("N");
+  test9.push("P");
+  test9.push("L");
+  testCrates.push(test9);
+  return testCrates;
 };
 
-const getInput = (): string[] => testInput.split("\n").map(String);
+const dataCreation = (inputArray: string[]): Instruction[] => {
+  let formattedInstructions: Instruction[] = [];
+  inputArray.forEach((instruction) => {
+    const numbersOnlyString = instruction.match(/\d+/g);
+
+    formattedInstructions.push({
+      iterations: +numbersOnlyString[0],
+      from: +numbersOnlyString[1],
+      to: +numbersOnlyString[2],
+    });
+  });
+  return formattedInstructions;
+};
+
+const dayFiveA = (inputArray: string[]): string => {
+  const instructions = dataCreation(inputArray);
+  let crates = cratesArray();
+  instructions.forEach((instruction) => {
+    for (let index = 0; index < instruction.iterations; index++) {
+      crates[instruction.to - 1].push(
+        crates[instruction.from - 1][crates[instruction.from - 1].length - 1]
+      );
+      crates[instruction.from - 1].pop();
+    }
+  });
+
+  let topCrates = "";
+  crates.forEach((crate) => {
+    topCrates += crate[crate.length - 1];
+  });
+  return topCrates;
+};
+
+const dayFiveB = (inputArray: string[]): string => {
+  const instructions = dataCreation(inputArray);
+  let crates = cratesArray();
+  instructions.forEach((instruction) => {
+    const test = crates[instruction.from - 1].slice(-instruction.iterations);
+    console.log("test", test);
+    test.forEach((element) => {
+      crates[instruction.to - 1].push(element);
+    });
+    for (let index = 0; index < instruction.iterations; index++) {
+      crates[instruction.from - 1].pop();
+    }
+  });
+
+  console.log("crates", crates);
+  let topCrates = "";
+  crates.forEach((crate) => {
+    topCrates += crate[crate.length - 1];
+  });
+  return topCrates;
+};
+
+const getInput = (): string[] => input.split("\n").map(String);
 
 export default function Day01() {
   return (
