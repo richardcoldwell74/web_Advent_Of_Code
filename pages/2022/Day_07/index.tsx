3313cb83e0ab48abc4b3c0f106c5883666c4b23a
richardcoldwell74 <76210848+richardcoldwell74@users.noreply.github.com>
2022-12-08 00:05:23 +0000

    it workd but my code makes me sick

--- pages/2022/Day_07/index.tsx
+++ pages/2022/Day_07/index.tsx
@@ -26,9 +26,9 @@ type Directory = {
 };
 type File = { parent: Directory; name: string; size: number };
 
-let ListOfActualDirectories: Directory[] = [];
-
 const daySevenA = (inputArray: string[]): number => {
+  let ListOfActualDirectories: Directory[] = [];
+
   const rootDirectory: Directory = {
     parent: null,
     name: "/",
@@ -100,9 +100,83 @@ const daySevenA = (inputArray: string[]): number => {
 };
 
 const daySevenB = (inputArray: string[]): number => {
-  let ret = 0;
+  let ListOfActualDirectories: Directory[] = [];
+  const rootDirectory: Directory = {
+    parent: null,
+    name: "/",
+    children: [],
+    files: [],
+    size: 0,
+  };
 
-  return ret;
+  ListOfActualDirectories.push(rootDirectory);
+
+  let currentDirectory: Directory = rootDirectory;
+  inputArray.forEach((command, index) => {
+    if (index > 1) {
+      if (command.includes("$ cd")) {
+        if (command.includes("..")) {
+          // go up a directory
+          currentDirectory = currentDirectory.parent;
+        } else {
+          // go down a directory
+          currentDirectory = currentDirectory.children.find((child) => {
+            return child.name === command.split(" ")[2];
+          }) as Directory;
+        }
+      } else if (command.includes("$ ls")) {
+        // do nothing for now
+      } else if (command.includes("dir")) {
+        //create  a directory as a child of the current directory
+        currentDirectory.children.push({
+          parent: currentDirectory,
+          name: command.split(" ")[1],
+          children: [],
+          files: [],
+          size: 0,
+        } as Directory);
+        ListOfActualDirectories.push(
+          currentDirectory.children[currentDirectory.children.length - 1]
+        );
+      } else {
+        // create a file as a child of the current directory
+        currentDirectory.files.push({
+          parent: currentDirectory,
+          name: command.split(" ")[1],
+          size: +command.split(" ")[0],
+        } as File);
+      }
+    }
+  });
+
+  ListOfActualDirectories.forEach((directory) => {
+    directory.size = directory.files.reduce((acc, file) => acc + file.size, 0);
+  });
+
+  for (let index = ListOfActualDirectories.length - 1; index >= 0; index--) {
+    ListOfActualDirectories[index].children.forEach((child) => {
+      ListOfActualDirectories[index].size += child.size;
+    });
+  }
+
+  console.log("ListOfActualDirectories", ListOfActualDirectories);
+
+  let total = 0;
+  ListOfActualDirectories.forEach((directory) => {
+    if (directory.size <= 100000) {
+      total += directory.size;
+    }
+  });
+  const spaceNeeded = 30000000 - (70000000 - rootDirectory.size);
+  const toDelete = ListOfActualDirectories.filter(
+    (directory) => directory.size >= spaceNeeded
+  );
+
+  toDelete.sort((a, b) => a.size - b.size); // b - a for reverse sort
+
+  console.log("toDelete", toDelete);
+
+  return toDelete[0].size;
 };
 
 const getInput = (): string[] => input.split("\n").map(String);
