4ffc2463d99a49507eb7295bcbd28c20f1170b8c
FrozenWellStudios <richardcoldwell74@gmail.com>
2022-12-22 19:27:52 +0000

    day 9 part 1

--- pages/2022/Day_09/index.tsx
+++ pages/2022/Day_09/index.tsx
@@ -18,17 +18,55 @@ const Container = styled.div`
 `;
 
 type Motion = { direction: string; steps: number };
+type Position = { x: number; y: number };
 
 const dayNineA = (inputArray: string[]): number => {
+  let tailVisited: Position[] = [];
+  let H = [0, 0];
+  let T = [0, 0];
   const motions: Motion[] = [];
   inputArray.forEach((line) => {
     motions.push({ direction: line.split(" ")[0], steps: +line.split(" ")[1] });
   });
-  console.log("motions", motions);
 
-  let total = 0;
+  motions.forEach((motion) => {
+    for (let index = 0; index < motion.steps; index++) {
+      let dx = 0;
+      let dy = 0;
+      if (motion.direction === "R") {
+        dx = 1;
+      } else if (motion.direction === "L") {
+        dx = -1;
+      } else if (motion.direction === "U") {
+        dy = 1;
+      } else if (motion.direction === "D") {
+        dy = -1;
+      }
+      H[0] += dx;
+      H[1] += dy;
 
-  return total;
+      const x = H[0] - T[0];
+      const y = H[1] - T[1];
+
+      if (Math.abs(x) > 1 || Math.abs(y) > 1) {
+        if (x === 0) {
+          T[1] += Math.sign(y);
+        } else if (y === 0) {
+          T[0] += Math.sign(x);
+        } else {
+          T[0] += Math.sign(x);
+          T[1] += Math.sign(y);
+        }
+      }
+      tailVisited.push({ x: T[0], y: T[1] });
+    }
+  });
+  tailVisited = tailVisited.filter(
+    (value, index, self) =>
+      index === self.findIndex((t) => t.x === value.x && t.y === value.y)
+  );
+
+  return tailVisited.length;
 };
 
 const dayNineB = (inputArray: string[]): number => {
@@ -39,7 +77,7 @@ const dayNineB = (inputArray: string[]): number => {
   return total;
 };
 
-const getInput = (): string[] => testInput.split("\n").map(String);
+const getInput = (): string[] => input.split("\n").map(String);
 
 export default function Day09() {
   return (
