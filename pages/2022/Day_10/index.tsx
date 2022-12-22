bb201ef7701b19d4df7d8fb4032a98b3c2d42660
FrozenWellStudios <richardcoldwell74@gmail.com>
2022-12-22 22:15:16 +0000

    day 10 part 2 works enough to get the answer

--- pages/2022/Day_10/index.tsx
+++ pages/2022/Day_10/index.tsx
@@ -21,7 +21,6 @@ const Container = styled.div`
 type Cycle = { cycleNumber: number; x: number };
 
 const dayTenA = (inputArray: string[]): number => {
-  console.log("inputArray", inputArray);
   const cycles: Cycle[] = [];
   let noOfCycles = 0;
   let x = 1;
@@ -44,27 +43,69 @@ const dayTenA = (inputArray: string[]): number => {
   totalSignalStrengths += cycles[139].cycleNumber * cycles[138].x;
   totalSignalStrengths += cycles[179].cycleNumber * cycles[178].x;
   totalSignalStrengths += cycles[219].cycleNumber * cycles[218].x;
-  console.log("cycles", cycles);
   return totalSignalStrengths;
 };
 
-const dayTenB = (inputArray: string[]): number => {
-  return 0;
+const dayTenB = (inputArray: string[]): string[] => {
+  console.log("inputArray", inputArray);
+  const cycles: Cycle[] = [];
+  let noOfCycles = 0;
+  let x = 1;
+  inputArray.forEach((line) => {
+    if (line.includes("noop")) {
+      noOfCycles += 1;
+      cycles.push({ cycleNumber: noOfCycles, x: x });
+    } else {
+      noOfCycles += 1;
+      cycles.push({ cycleNumber: noOfCycles, x: x });
+      noOfCycles += 1;
+      x += +line.split(" ")[1];
+      cycles.push({ cycleNumber: noOfCycles, x: x });
+    }
+  });
+
+  console.log("cycles", cycles);
+
+  let crtDisplay: string[] = [];
+  cycles.forEach((cycle) => {
+    if (
+      cycle.cycleNumber % 40 === cycle.x ||
+      cycle.cycleNumber % 40 === cycle.x + 1 ||
+      cycle.cycleNumber % 40 === cycle.x - 1
+    ) {
+      crtDisplay.push("#");
+    } else {
+      crtDisplay.push(".");
+    }
+  });
+  let splitCrtDisplay: string[] = [];
+
+  const chunkSize = 40;
+  for (let i = 0; i < crtDisplay.length; i += chunkSize) {
+    const chunk = crtDisplay.slice(i, i + chunkSize);
+    let characterString = "";
+    chunk.forEach((line) => {
+      characterString += line;
+    });
+    splitCrtDisplay.push(characterString);
+  }
+  console.log("splitCrtDisplay", splitCrtDisplay);
+  return crtDisplay;
 };
 
 const getInput = (): string[] => input.split("\n").map(String);
 
-export default function Day09() {
+export default function Day10() {
   return (
     <Container>
       <Head>
-        <title>AOC 2022 - Day 09</title>
+        <title>AOC 2022 - Day 10</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header />
-        <h1>Day 9</h1>
+        <h1>Day 10</h1>
         <h2>Part 1</h2>
         <p>{dayTenA(getInput())}</p>
         <h2>Part 2</h2>
